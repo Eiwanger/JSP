dde9bfe1f6dc330cf51556fbe120168cb27fd1af
Stefan <stefeiwanger@t-online.de>
2018-10-04 10:11:42 +0300

    update forum assignment

--- Assignment_Forum/src/forum/ForumServlet.java
+++ Assignment_Forum/src/forum/ForumServlet.java
@@ -14,16 +14,16 @@ public class ForumServlet extends HttpServlet {
         response.setContentType("text/html");
         PrintWriter out = response.getWriter();
         String bValue = request.getParameter("action");
-        ArrayList<Message> searchResults= new ArrayList<Message>();
+        ArrayList<Message> searchResults = new ArrayList<Message>();
 
-        if(bValue.equalsIgnoreCase("Search Name")){
+        if (bValue.equalsIgnoreCase("Search Name")) {
             String input = request.getParameter("sName");
             out.println(Message.printSearchedMessages(Message.searchName(input), input));
-        }else if(bValue.equalsIgnoreCase("Search Date")){
+        } else if (bValue.equalsIgnoreCase("Search Date")) {
             String input = request.getParameter("sDate");
             out.println(Message.printSearchedMessages(Message.searchDate(input), input));
         }
-   }
+    }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
@@ -31,18 +31,18 @@ public class ForumServlet extends HttpServlet {
         PrintWriter out = response.getWriter();
         String bValue = request.getParameter("action");
 
-        if(bValue.equals("Submit")) {
+        if (bValue.equals("Submit")) {
 
             String username;
             String message;
-            if(request.getParameter("userName").isEmpty()){
+            if (request.getParameter("userName").isEmpty()) {
                 username = "noUser";
-            }else{
+            } else {
                 username = request.getParameter("userName");
             }
-            if(request.getParameter("message").isEmpty()){
+            if (request.getParameter("message").isEmpty()) {
                 message = "noMessage";
-            }else{
+            } else {
                 message = request.getParameter("message");
             }
 
@@ -50,11 +50,11 @@ public class ForumServlet extends HttpServlet {
             Message.addToList(m);
             out.println(Message.printAllMessages());
 
-        }else if(bValue.equalsIgnoreCase("Delete Forum")) {
+        } else if (bValue.equalsIgnoreCase("Delete Forum")) {
             Message.deletePostList();
             out.println(Message.printAllMessages());
 
-        }else if(bValue.equalsIgnoreCase("Go to Forum")){
+        } else if (bValue.equalsIgnoreCase("Go to Forum")) {
 
             out.println(Message.printAllMessages());
         }
